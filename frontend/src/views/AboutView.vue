68101e19ecf10242c1aa88bf365cd4a4057c074c
AurLemon <aurlemoncn@gmail.com>
2025-04-16 03:42:28 +0800

    fix(frontend/about): 修复版本信息显示条件

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -20,7 +20,9 @@ export default defineComponent({
             <div class="page-about-github__name">FJCPC Transfer Exam Practice System</div>
             <div class="page-about-github__desc">给需要转轨考的朋友准备的喵</div>
             <div class="page-about-github__info">
-                <div class="page-about-github__version">v3.0.0 ({{ questionStore.questionInfo.git_info.current_commit.slice(0, 8) }})</div>
+                <div class="page-about-github__version" v-if="questionStore.questionInfo.git_info.local_commit">
+                    v3.0.0 ({{ questionStore.questionInfo.git_info.local_commit.slice(0, 8) }})
+                </div>
             </div>
             <div class="page-about-github__links">
                 <a
