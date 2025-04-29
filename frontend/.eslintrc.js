9c45d1fa37fd755e32597e03f8e052ede4192209
AurLemon <aurlemoncn@gmail.com>
2025-04-30 00:50:32 +0800

    chore(frontend): 更新 ESLint 配置

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,16 +1,29 @@
 module.exports = {
   root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   extends: [
-    'plugin:vue/essential',
     'eslint:recommended',
-    '@vue/prettier'
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@vue/eslint-config-prettier'
   ],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-    'indent': ['error', 2, { 
-      "ignoredNodes": ["TemplateLiteral", "JSXElement", "JSXAttribute", "Comment"] 
+    indent: ['error', 2, {
+      ignoredNodes: ['TemplateLiteral', 'JSXElement', 'JSXAttribute', 'Comment']
     }],
     'vue/html-indent': ['error', 2],
-    'prettier/prettier': ['error', { 
+    'prettier/prettier': ['error', {
       semi: false,
       singleQuote: true
     }]
