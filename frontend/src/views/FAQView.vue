baf62b97c2a17b0a4f1601fbac62f89d189f848c
AurLemon <aurlemoncn@gmail.com>
2025-04-30 16:17:29 +0800

    feat(frontend/faq): 调整内容

--- frontend/src/views/FAQView.vue
+++ frontend/src/views/FAQView.vue
@@ -6,6 +6,7 @@ import 'prismjs/themes/prism-tomorrow.css'
 
 <template>
   <div class="page-container-slide page-faq">
+    <div class="page-container-title">答疑</div>
     <FAQView />
   </div>
 </template>
@@ -14,10 +15,33 @@ import 'prismjs/themes/prism-tomorrow.css'
 .markdown-body {
   background-color: transparent;
 }
+
+.faq-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1em;
+
+  .faq-question {
+    color: var(--color-base--subtle);
+    margin: 0;
+    padding: 0 1em;
+    padding-left: 0.5em;
+    font-size: 0.875em;
+    border-left: 0.25em solid #d1d9e0;
+  }
+
+  .faq-answer {
+    margin-top: 0.25em;
+  }
+}
 </style>
 
 <style lang="scss" scoped>
 .page-faq {
   overflow-y: auto;
+
+  .page-container-title {
+    text-align: center;
+  }
 }
 </style>
